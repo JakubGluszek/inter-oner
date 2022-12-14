0305b7460fcd5eb729c2ed36b726410c9e100950
JacobG03 <jacobgluszek03@gmail.com>
2022-12-14 14:40:26 +0100

    map fix

--- components/GoogleMaps.tsx
+++ components/GoogleMaps.tsx
@@ -10,7 +10,7 @@ const GoogleMaps: React.FC = () => {
       scrolling="no"
       marginHeight={0}
       marginWidth={0}
-      src="https://maps.google.com/maps?width=100%25&amp;height=420&amp;hl=en&amp;q=ul.%20B.%20Prusa%2010+(Inter-oner)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
+      src="https://maps.google.com/maps?width=100%25&amp;height=420&amp;hl=en&amp;q=ul.%20B.%20Prusa%2010+(Inter-oner)+Bielsko-Biała&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
       width="100%"
       height={width >= 640 ? "100%" : 320}
       onClick={() => null}
